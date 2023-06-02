01e7814a16dbd9619f72420162231c30f590fa8b
dhalidi <daniel.halidi.iut@gmail.com>
2023-06-02 16:02:20 +0200

    Ajout du formulaire dans le fichier admin

--- admin.php
+++ admin.php
@@ -45,35 +45,71 @@ if (!isset($_SESSION['admin']) || $_SESSION['admin'] !== true) {
 
     <h2 class="hautpage">Administration des batiments</h2>
 
-<section class="admin">
+    <section class="admin">
+        <legend class="form">Choissisez le bâtiment que vous souhaitez ajouter</legend>
+        <form action="#" method="post" enctype="multipart/form-data">
+            <section class="batiment">
+                
+                    <input type="radio" id="A" name="bat_add" value="Batiment A" class="radio-input">
+                    <label for="A" class="nomBAT">Batiment A</label><br>
+                    <input type="radio" id="B" name="bat_add" value="Batiment B" class="radio-input">
+                    <label for="B" class="nomBAT">Batiment B</label><br>
+                    <input type="radio" id="C" name="bat_add" value="Batiment C" class="radio-input">
+                    <label for="C" class="nomBAT">Batiment C</label><br>
+                    <input type="radio" id="D" name="bat_add" value="Batiment D" class="radio-input">
+                    <label for="D" class="nomBAT">Batiment D</label><br>
+            </section>
 
-    <form action="#" method="post" enctype="multipart/form-data">
-        <fieldset class="batiment">
-            <legend>Choissisez le bâtiment que vous souhaitez ajouter</legend>
-        </fieldset>
+        <section class="form">
+                <label for="nom" class="texte">Nom du batiment</label><br>
+                <input type="text" id="nom" name="nom" value=""><br>
 
-        <section class="valid">
-            <input type="submit" value="Valider" />
+                <label for="login" class="texte">Login</label><br>
+                <input type="text" id="login" name="login" value=""><br>
+
+                <label for="nom" class="texte">Password</label><br>
+                <input type="text" id="mdp" name="mdp" value=""><br>
         </section>
-    </form>
+            <section class="valid">
+                <input type="submit" value="Valider" class="valider" />
+            </section>
+        </form>
+    
+    </section>
 
-</section>
 
+    <section class="admin">
 
+        <form action="#" method="post" enctype="multipart/form-data">
+            <fieldset class="batiment">
+                <legend>Choissisez le bâtiment que vous souhaitez supprimer</legend>
+    
+                    <input type="radio" id="A" name="bat_add" value="Batiment A" class="radio-input">
+                    <label for="A" class="nomBAT">Batiment A</label><br>
+                    <input type="radio" id="B" name="bat_add" value="Batiment B" class="radio-input">
+                    <label for="B" class="nomBAT">Batiment B</label><br>
+                    <input type="radio" id="C" name="bat_add" value="Batiment C" class="radio-input">
+                    <label for="C" class="nomBAT">Batiment C</label><br>
+                    <input type="radio" id="D" name="bat_add" value="Batiment D" class="radio-input">
+                    <label for="D" class="nomBAT">Batiment D</label><br>
+            </fieldset>
+            
+                <label for="nom">Nom du batiment</label><br>
+                <input type="text" id="nom" name="nom" value=""><br>
 
-<section class="admin">
+                <label for="login">Login</label><br>
+                <input type="text" id="login" name="login" value=""><br>
 
-    <form action="#" method="post" enctype="multipart/form-data">
-        <fieldset class="batiment">
-            <legend>Choissisez le bâtiment que vous souhaitez supprimer</legend>
-        </fieldset>
+                <label for="nom">Password</label><br>
+                <input type="text" id="mdp" name="mdp" value=""><br>
 
-        <section class="valid">
-            <input type="submit" value="Valider" />
-        </section>
-    </form>
-
-</section>
+            
+            <section class="valid">
+                <input type="submit" value="Valider" />
+            </section>
+        </form>
+    
+    </section>
 
 
 <section class="TITREBAS">
@@ -85,7 +121,15 @@ if (!isset($_SESSION['admin']) || $_SESSION['admin'] !== true) {
 
     <form action="#" method="post" enctype="multipart/form-data">
         <fieldset class="batiment">
-            <legend>Choissisez le bâtiment que vous souhaitez ajouter</legend>
+            <legend>Choissisez le capteur que vous souhaitez ajouter</legend>
+                <input type="radio" id="A" name="bat_add" value="Batiment A" class="radio-input">
+                <label for="A" class="nomBAT">Batiment A</label><br>
+                <input type="radio" id="B" name="bat_add" value="Batiment B" class="radio-input">
+                <label for="B" class="nomBAT">Batiment B</label><br>
+                <input type="radio" id="C" name="bat_add" value="Batiment C" class="radio-input">
+                <label for="C" class="nomBAT">Batiment C</label><br>
+                <input type="radio" id="D" name="bat_add" value="Batiment D" class="radio-input">
+                <label for="D" class="nomBAT">Batiment D</label><br>
         </fieldset>
 
         <section class="valid">
@@ -99,7 +143,15 @@ if (!isset($_SESSION['admin']) || $_SESSION['admin'] !== true) {
 
     <form action="#" method="post" enctype="multipart/form-data">
         <fieldset class="batiment">
-            <legend>Choissisez le bâtiment que vous souhaitez supprimer</legend>
+            <legend>Choissisez le capteur que vous souhaitez supprimer</legend>
+                <input type="radio" id="A" name="bat_add" value="Batiment A" class="radio-input">
+                <label for="A" class="nomBAT">Batiment A</label><br>
+                <input type="radio" id="B" name="bat_add" value="Batiment B" class="radio-input">
+                <label for="B" class="nomBAT">Batiment B</label><br>
+                <input type="radio" id="C" name="bat_add" value="Batiment C" class="radio-input">
+                <label for="C" class="nomBAT">Batiment C</label><br>
+                <input type="radio" id="D" name="bat_add" value="Batiment D" class="radio-input">
+                <label for="D" class="nomBAT">Batiment D</label><br>
         </fieldset>
 
         <section class="valid">
